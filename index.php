6378d39463a792319dca1fd722b2ec93cfb6d250
Mikko Paulo Guevarra <emphigie10@gmail.com>
2021-01-16 00:01:15 +0800

    i tried

--- index.php
+++ index.php
@@ -3,16 +3,26 @@
 class Movie {
     public $title;
     public $genre;
+    public $orig_lang;
     public $language;
 
-    function __construct($_title, $_genre, $_language) {
+    function __construct($_title, $_genre, $_orig_lang) {
         $this->title = $_title;
         $this->genre = $_genre;
-        $this->language = $_language;
+        $this->orig_lang = $_orig_lang;
+    }
+
+    public function setLang($language) {
+        return $this->language = $language;
     }
 }
 
 $movie1 = new Movie('avengers', 'action', 'english');
+$movie1_title = $movie1->title;
+$movie1_genre = $movie1->genre;
+$movie1_orig_lang = $movie1->orig_lang;
+
+$movie1->setLang('english');
 
 
  ?>
@@ -25,6 +35,16 @@ $movie1 = new Movie('avengers', 'action', 'english');
     </head>
     <body>
         <h1>Object Oriented Programing</h1>
-        
+        <h2>Movies</h2>
+        <p> Title:
+            <?php echo $movie1_title?>
+        </p>
+        <p> Genre:
+            <?php echo $movie1_genre?>
+        </p>
+        <p> Original Lang:
+            <?php echo $movie1_orig_lang?>
+        </p>
+
     </body>
 </html>
